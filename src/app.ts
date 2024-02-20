9c25cfb4d066406fa94f1de1436364e02ea97a1a
builtbysuraj <surajgupta3553@gmail.com>
2024-02-20 18:40:40 +0530

    Refactor cors

--- src/app.ts
+++ src/app.ts
@@ -6,25 +6,13 @@ import router from './routes/user.routes'
 
 const app = express()
 
-// app.use(cors())
-// 👇️ specify origins to allow
-const whitelist = ['http://localhost:5173', 'http://localhost:3000']
+app.use(
+  cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+  })
+)
 
-// ✅ Enable pre-flight requests
-app.options('*', cors())
-
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-}
-
-app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
