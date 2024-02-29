05dd48526635c4beecc98a876125434f03c8b22a
builtbysuraj <surajgupta3553@gmail.com>
2024-02-29 20:31:42 +0530

    remove secure from cookies option

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -32,8 +32,8 @@ export const loginUser = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: true,
-      maxAge: 24 * 60 * 60 * 1000, // Expires in 1 day
+      // secure: true,
+      // maxAge: 24 * 60 * 60 * 1000, // Expires in 1 day
       // sameSite: 'none',
     })
     return res.json({
--- src/controller/register.controller.ts
+++ src/controller/register.controller.ts
@@ -31,9 +31,9 @@ export const registerUser = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: true,
+      // secure: true,
       // sameSite: 'none',
-      maxAge: 24 * 60 * 60 * 1000, // Expires in 1 day
+      // maxAge: 24 * 60 * 60 * 1000, // Expires in 1 day
     })
     return res.json({ message: 'User registered successfully', token })
   } catch (error) {
