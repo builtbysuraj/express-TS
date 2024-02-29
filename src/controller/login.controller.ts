029791402e83f432e6414396a23c3ec6af70c1e5
builtbysuraj <surajgupta3553@gmail.com>
2024-02-29 20:47:01 +0530

    refactor

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -32,9 +32,9 @@ export const loginUser = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      // secure: true,
+      secure: false,
       // maxAge: 24 * 60 * 60 * 1000, // Expires in 1 day
-      // sameSite: 'none',
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     })
     return res.json({
       message: 'Login successful',
